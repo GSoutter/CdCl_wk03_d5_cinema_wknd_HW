3ffe2f8ec8574088d8d77df3f697372d0adf629f
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-27 16:54:37 +0000

    adds update function to customer, film, ticket classes.

--- models/ticket.rb
+++ models/ticket.rb
@@ -34,6 +34,12 @@ class Ticket
     SqlRunner.run(sql, [])
   end
 
+  def update()
+    sql = "UPDATE tickets SET (customer_id, film_id)=($1,$2) WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 end #class end
